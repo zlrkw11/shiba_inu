51f5b363937b3be29ad3a73088bcc0201718abbb
zlrkw11 <ray040406@gmail.com>
2025-02-10 18:15:37 +1100

    4 pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 export default function Home() {
   return (
-    <main className="bg-scroll flex flex-col items-center justify-center h-screen min-h-screen">
+    <main className="bg-scroll flex flex-col items-center justify-center min-h-screen">
       <section
         id="home"
         className="h-screen flex flex-col items-center justify-center"
@@ -9,6 +9,26 @@ export default function Home() {
         <h1 className="display-1 fw-bold">Shiba Inu</h1>
         <hr className="w-1/2 border-t-2 border-gray-400 my-4" />
         <p className="lead">For students, by students</p>
+      </section>
+      <section
+        id="tutors"
+        className="h-screen flex flex-col items-center justify-center"
+      >
+        <h1 className="display-1 fw-bold">Tutors</h1>
+      </section>
+      <section
+        id="book"
+        className="h-screen flex flex-col items-center justify-center"
+      >
+        <h1 className="display-1 fw-bold">Book Now!</h1>
+      </section>{" "}
+      <section
+        id="contact"
+        className="h-screen flex flex-col items-center justify-center"
+      >
+        <h1 className="display-1 fw-bold">Get in touch</h1>
+        <p className="lead">Ray Zhao</p>
+        <p className="lead">email: Ray040406@gmail.com</p>
       </section>
     </main>
   );
