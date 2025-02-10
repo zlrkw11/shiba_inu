a3b36e134528c55d501da6123cd9de79a320c890
zlrkw11 <ray040406@gmail.com>
2025-02-10 18:42:36 +1100

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-bs-theme="dark">
+    <html lang="en" data-bs-theme="">
       <body className="bg-scroll">
         <Navbar />
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import "./globals.css";
+import Image from "next/image";
+import doge from "./doge-removebg-preview.png";
 export default function Home() {
   return (
     <main className="bg-scroll flex flex-col items-center justify-center min-h-screen">
@@ -8,6 +10,7 @@ export default function Home() {
       >
         <h1 className="display-1 fw-bold">Shiba Inu</h1>
         <hr className="w-1/2 border-t-2 border-gray-400 my-4" />
+        <Image src={doge} alt="Shiba Inu" />
         <p className="lead">For students, by students</p>
       </section>
       <section
@@ -29,7 +32,13 @@ export default function Home() {
         <h1 className="display-1 fw-bold">Get in touch</h1>
         <p className="lead">Ray Zhao</p>
         <p className="lead">email: Ray040406@gmail.com</p>
+        <p className="lead">Eric Cai</p>
+        <p className="lead">email: example email here</p>
+        <p>Address: boxhill central melbourne VIC</p>
       </section>
+      <footer className="bg-orange-400 text-white w-full p-4 text-center">
+        © Copyright 2025 ShibaInu | 434 152 176 | shibainututoring@gmail.com
+      </footer>
     </main>
   );
 }
